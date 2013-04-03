318df976c1009d4c29efe36c04e397ff7ac5972f
Adam Borocz <boroczadam@gmail.com>
2013-04-03 14:15:00 +0100

    Update specs to work from Strainer sandbox

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require "chefspec"
-COOKBOOK_PATH = "cookbook_test"
 
 CHEF_RUN_OPTIONS = {
-  :cookbook_path => COOKBOOK_PATH,
   :platform => 'ubuntu',
   :version => '12.04',
   :log_level => :fatal
