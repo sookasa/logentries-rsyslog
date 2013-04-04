d3ed3d8b5cbc4eb955771ae72dfdee841fb28f34
Adam Borocz <boroczadam@gmail.com>
2013-04-04 17:08:17 +0100

    Add platform support to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          "Apache 2.0"
 description      "Installs/Configures Logentries for Rsyslog"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+supports 'ubuntu', ">= 12.04"
 
 depends "rsyslog", "~> 1.5.0"
 
